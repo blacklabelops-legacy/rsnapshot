caf7d6bace2ebdbbabdf14ac61023b2572146bf2
Steffen Bleul <blacklabelops@itbleul.de>
2015-08-15 17:17:25 +0200

    Added path.

--- rsnapshot-cron/docker-entrypoint.sh
+++ rsnapshot-cron/docker-entrypoint.sh
@@ -42,6 +42,7 @@ if [ -n "${CRON_LOG_FILE}" ]; then
 fi
 
 crontab <<EOF
+PATH=/opt/rsnapshot:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 ${cron_rsnapshot_hourly} /usr/bin/rsnapshot hourly ${cronlog_command}
 ${cron_rsnapshot_daily} /usr/bin/rsnapshot daily ${cronlog_command}
 ${cron_rsnapshot_weekly} /usr/bin/rsnapshot weekly ${cronlog_command}
